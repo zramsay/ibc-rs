1134fcbeba896181b61cd4ae53823bd73318cbef
Andy Nogueira <me@andynogueira.dev>
2020-07-10 16:36:51 -0400

    Testing using rust-protobuf to generate the rs code

--- modules/build.rs
+++ modules/build.rs
@@ -1,5 +1,18 @@
 extern crate prost_build;
+extern crate protoc_rust;
+
+use protoc_rust::Customize;
 
 fn main() {
+
     prost_build::compile_protos(&["src/proto/connection.proto"], &["src/proto"]).unwrap();
+
+    protoc_rust::run(protoc_rust::Args {
+        out_dir: "src/proto",
+        input: &["src/proto/connection.proto"],
+        includes: &["src/proto"],
+        customize: Customize {
+            ..Default::default()
+        },
+    }).expect("protoc");
 }
